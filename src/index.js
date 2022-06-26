28e18f712d96bd98759b567ea13da95ecff4c496
Gremlin <50713742+GremlinX@users.noreply.github.com>
2022-06-25 22:40:48 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Pizza</li>
+      <li>Hamburguer</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
