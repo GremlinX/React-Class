7cb6ead6e505d986fca82330c7264d36481759fd
Gremlin <50713742+GremlinX@users.noreply.github.com>
2022-06-25 22:56:53 -0300

    Class 396 Implement exercise from class 396

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Variables needs to be wrapped inside curly braces
+// in order to get rendered.
+const name = "Gremlin";
+const title = "the dev";
+const number = "1";
+
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Pizza</li>
-      <li>Hamburguer</li>
-    </ul>
+    <h1>Hello {name + ", " + title}!</h1> 
+    {/* <h1>Hello {name} {title}!</h1> */}
+    {/* <h1>Hello {`${name} ${title}`}!</h1> */}
+    <p>Your lucky number is: {number}!</p>
   </div>,
   document.getElementById("root")
 );
