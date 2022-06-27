de5fa1a4dbdb0820efb9745095d87cdbda40e2dc
Gremlin <50713742+GremlinX@users.noreply.github.com>
2022-06-26 21:51:47 -0300

    409 Class Solve React challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Heading from "./Heading";
-import Images from "./Images";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
 
 function App() {
   return (
     <div>
-      <Heading />
-      <div>
-        <Images />
-      </div>
+      <Header />
+      <Note />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+var date = new Date();
+var year = date.getFullYear();
+
+function Footer() {
+  return <footer>Copyright {year}</footer>;
+}
+
+export default Footer;
